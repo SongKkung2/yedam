b994815da386370d76358227913d6957e2022435
sujin3 <elwkdlsj03@naver.com>
2022-06-13 12:41:12 +0900

    다조사why animate 세팅

---
+++ script/reason.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $(window).scroll( function(){
+        $('.animate__fadeInUp').each(function(i){
+            
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            if( bottom_of_window > bottom_of_element ){
+                $(this).animate({'margin-bottom':'0px'},800);
+            }
+            
+        }); 
+    });
+});
