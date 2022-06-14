ea1c71b782ef18a434a22aedd241c3341a2484a7
chaeeun song <mac@Macui-MacBook-Pro.local>
2022-06-14 22:44:11 +0900

    연혁 페이지 50% 완성

--- js/global.js
+++ js/global.js
@@ -1,9 +1,5 @@
 $(function(){
     menu();
-    let intervalID = setInterval(slide,5000);
-    let interval2ID = setInterval(slideM,5000);
-    slide2();
-    animatescroll();
     mobileMenu();
     burger();
 })
@@ -31,45 +27,8 @@ function menu(){
     
 }
 
-function slide(){
-    let CurrItem = $("#items > li.top")
-    let NextItem = CurrItem.next();
 
-    if(!NextItem.length){NextItem = $("#items > li:first")}
 
-    CurrItem.removeClass("top")
-    NextItem.addClass("top")
-}
-function slideM(){
-    let CurrItem = $("#items2 > li.top")
-    let NextItem = CurrItem.next();
-
-    if(!NextItem.length){NextItem = $("#items2 > li:first")}
-
-    CurrItem.removeClass("top")
-    NextItem.addClass("top")
-}
-
-function slide2(){
-    // let nowimg = $("#about_items > li.on");
-    // let nextimg = nowimg.next();
-
-    // if(!nextimg.length){
-    //     nextimg = $("#about_items > li:first")
-    // }
-
-    $('#play').click(function(){
-        let nowimg = $("#about_items > li.on");
-        let nextimg = nowimg.next();
-
-        if(!nextimg.length){
-            nextimg = $("#about_items > li:first")
-        }
-
-        nowimg.removeClass("on")
-        nextimg.addClass("on")
-    })
-}
 
 // $(function(){
 //     //Keep track of last scroll
@@ -99,28 +58,6 @@ $(window).scroll(function() {
 
 
 
-function animatescroll(){
-    $(window).scroll(function () { 
-        var scrollV = $(document).scrollTop(); 
-        // console.log(scrollV);
-        let offText = $('.text-wrap').offset().top;
-        let offCourse = $('.course-contents li').offset().top;
-        // console.log(off);
-    
-        let valueText = scrollV - offText
-        let valueCourse = scrollV - offCourse
-        console.log(valueCourse);
-    
-        if(valueText > 1300){
-            $('.text-wrap').addClass('animate__animated')
-            $('.text-wrap').removeClass('P-none')
-        }
-        if(valueCourse > 1700){
-            $('.course-contents li').addClass('animate__animated')
-            $('.course-contents li').removeClass('P-none')
-        }
-    });
-}
 
 function mobileMenu(){
     $('.burger').click(function(){
@@ -128,10 +65,10 @@ function mobileMenu(){
         $('.burger-close').fadeIn();
         $('.gnb-mobile-wrap').slideDown();
 
-        // $('body').css({'overflow':'hidden'});
-        // $(document).bind('scroll',function () {
-        //     window.scrollTo(0,0);
-        // });
+        $('body').css({'overflow':'hidden'});
+        $(document).bind('scroll',function () {
+            window.scrollTo(0,0);
+        });
 
     })
     $('.burger-close').click(function(){
@@ -139,8 +76,8 @@ function mobileMenu(){
         $('.burger-close').fadeOut();
         $('.gnb-mobile-wrap').slideUp();
 
-        // $(document).unbind('scroll');
-        // $('body').css({'overflow':'visible'});
+        $(document).unbind('scroll');
+        $('body').css({'overflow':'visible'});
 
     })
     
